d1c05f65329512418b0510dbeedef5efe804c1e0
guilhermerc <guilherme.riciolic@gmail.com>
2018-04-27 14:21:16 -0300

    Subindo tratamento_final.py funcional

--- workspace/src/python/tratamento_final.py
+++ workspace/src/python/tratamento_final.py
@@ -10,18 +10,18 @@ except:
     print('Usage: data_base.csv')
     exit()
 
-with open(input_file, newline='') as csv_file:
-
-    database = csv.reader(csv_file, delimiter=',', quotechar='"')
-
-    for line in sys.stdin:
-        fields = line.split('-')
-        fields[3].rstrip()
-        fields[2].replace(',', '.')
-        fields[3].replace(',', '.')
+# itera nas linhas da entrada padrao
+for line in sys.stdin:
+    fields = line.split('-')
+    fields[3] = fields[3].rstrip()  # retira '\n' do final
+    fields[2].replace(',', '.')
+    fields[3].replace(',', '.')
+    with open(input_file, newline='') as csv_file:
+        database = csv.reader(csv_file, delimiter=',', quotechar='"')
+        # itera nas linhas do csv
         for row in database:
             if fields[0] == row[1]:
                 fields[0] = row[0]
             if fields[1] == row[1]:
                 fields[1] = row[0]
-        print(fields[0] + "," + fields[1] + "," + fields[2] + "," + fields[3])
+    print(fields[0] + "," + fields[1] + "," + fields[2] + "," + fields[3])
