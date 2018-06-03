bdae335b43deba75e86a0a8078fd2aee6c169f66
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-03 16:06:46 -0300

    Uploading convolution library. Single line convolution (aka dot product) function implemented, but not tested yet.

---
+++ project_02/workspace/lib/convolution_lib.c
@@ -0,0 +1,65 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "convolution_lib.h"
+#include "image_io_lib.h"
+
+typedef struct
+{
+    uint16_t order;
+    uint16_t divider;
+    float ** matrix;
+} conv_matrix;
+
+typedef struct
+{
+    uint16_t currently_row;
+    uint16_t currently_column;
+} currently_position;
+
+// This function calculates a single row of a convolution between two matrices.
+// It is assumed that the row passed as an argument is valid
+double calculate_conv_row(image * img, float * channel, image * tmp, conv_matrix
+        * cm, uint16_t pixel_row, uint16_t pixel_column, uint16_t cm_row)
+{
+    // Orthogonal distance from the borders of the convolution matrix to the
+    // pixel under calcultation
+    uint16_t distance = cm.order / 2;
+
+    // Imagine we superpose the convolution matrix over the image matrix,
+    // centering the former matrix in the currently pixel under calculation of
+    // the latter. Then, the position (0, 0) of the convolution matrix is
+    // correlated to the following position in the image matrix 
+    currently_position img_position;
+    // The convolution image row never changes in this context, since this
+    // function calculates a single row of convolution operation
+    img_position.currently_row = pixel_row - distance;
+    img_position.currently_column = pixel_column - distance;
+
+    currently_position cm_position;
+    // Never changes in this context
+    cm_position.currently_row = cm_row;
+    cm_position.currently_column = 0;
+
+    double convolution_row_sum = 0.0;
+    // Calculates the dot product between the image and the convolution matrix
+    // respective rows
+    while(cm_position.currently_column < cm.order)
+    {
+        // If the convolution image position is inside of the image matrix, the
+        // the convolution operation is valid. We decided to ignore those cases
+        // in the filter aplication. It'll cause a border-effect: fading pixels
+        // near to the borders).
+        if(img_position.currently_row >= 0 &&
+                img_position.currently_column >= 0)
+        {
+            convolution_row_sum += channel[array_index(img,
+                    img_position.currently_row, img_position.currently_column)]
+                * cm.matrix[ cm_position.currently_row,
+                cm_position.currently_column];
+        }
+        img_position.currently_column++;
+        cm_position.currently_column++;
+    }
+    return convolution_row_sum;
+} 
---
+++ project_02/workspace/lib/convolution_lib.h
@@ -0,0 +1,13 @@
+#ifndef CONVOLUTION_LIB_H
+#define CONVOLUTION_LIB_H
+
+typedef struct
+{
+    uint16_t order;
+    uint16_t divider;
+    float ** matrix;
+} conv_matrix;
+
+double calculate_conv_row(image * img, float * channel, image * tmp, conv_matrix
+        * cm, uint16_t pixel_row, uint16_t pixel_column, uint16_t cm_row);
+#endif
