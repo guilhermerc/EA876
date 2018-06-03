3c09c460c7155277cdfb6c1d836d74152890c85b
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-03 18:06:23 -0300

    Correcly accessing matrix elements

--- project_02/workspace/lib/convolution_lib.c
+++ project_02/workspace/lib/convolution_lib.c
@@ -1,26 +1,27 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "convolution_lib.h"
 #include "image_io_lib.h"
+#include "convolution_lib.h"
 
 typedef struct
 {
-    uint16_t order;
-    uint16_t divider;
-    float ** matrix;
-} conv_matrix;
-
-typedef struct
-{
-    uint16_t currently_row;
-    uint16_t currently_column;
+    int16_t currently_row;
+    int16_t currently_column;
 } currently_position;
+
+// This function calculates the index of a continuous matrix representation as
+// an array, given its correponding (i, j) position.
+uint64_t array_matrix_index(image img, uint16_t row, uint16_t column)
+{
+    uint64_t index = row * img.width + column;	
+    return index;
+}
 
 // This function calculates a single row of a convolution between two matrices.
 // It is assumed that the row passed as an argument is valid
-double calculate_conv_row(image * img, float * channel, image * tmp, conv_matrix
-        * cm, uint16_t pixel_row, uint16_t pixel_column, uint16_t cm_row)
+double calculate_conv_row(image img, float * channel, image * tmp, conv_matrix
+        cm, uint16_t pixel_row, uint16_t pixel_column, uint16_t cm_row)
 {
     // Orthogonal distance from the borders of the convolution matrix to the
     // pixel under calcultation
@@ -53,13 +54,13 @@ double calculate_conv_row(image * img, float * channel, image * tmp, conv_matrix
         if(img_position.currently_row >= 0 &&
                 img_position.currently_column >= 0)
         {
-            convolution_row_sum += channel[array_index(img,
+            convolution_row_sum += channel[array_matrix_index(img,
                     img_position.currently_row, img_position.currently_column)]
-                * cm.matrix[ cm_position.currently_row,
-                cm_position.currently_column];
+                    * cm.matrix[cm_position.currently_row][cm_position.currently_column];
         }
         img_position.currently_column++;
         cm_position.currently_column++;
     }
     return convolution_row_sum;
 } 
+
