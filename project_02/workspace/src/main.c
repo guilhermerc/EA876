ea126a667e96ee3f1bf78ec4cbb3d8f1e2dabe61
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-04 01:18:06 -0300

    Removing debug code

--- project_02/workspace/src/main.c
+++ project_02/workspace/src/main.c
@@ -6,7 +6,8 @@
 
 int main(int argc, char ** argv)
 {
-        image img = open_image("tests/theo.jpg");
+        image img = open_image("tests/test.jpg");
+
         image tmp;
         tmp.width = img.width;
         tmp.height = img.height;
@@ -16,43 +17,21 @@ int main(int argc, char ** argv)
         tmp.b = (float *) malloc(sizeof(float) * tmp.width * tmp.height);
         for(int i = 0; i < tmp.width * tmp.height; i++) 
         {
-            tmp.r[i] = 0;
-            tmp.g[i] = img.g[i];
-            tmp.b[i] = img.b[i];
+            tmp.r[i] = 0; tmp.g[i] = 0; tmp.b[i] = 0;
         }
+
         conv_matrix cm;
-        cm.order = 20; cm.divider = cm.order * cm.order;
+        cm.order = 10; cm.divider = 100;
 
         cm.matrix = malloc(sizeof(float *) * cm.order);
-        for(int i = 0; i < cm.order; i ++)
+        for(int i = 0; i < cm.order; i++)
             cm.matrix[i] = malloc(sizeof(float) * cm.order);
 
         for(int i = 0; i < cm.order; i++)
         {
-            for(int j = 0; j < cm.order; j++) cm.matrix[i][j] = 1;
+            for(int j = 0; j < cm.order; j++)
+                cm.matrix[i][j] = 1;
         }
-
-        /* 
-         
-        for(int i = 0; i < img.height; i++)
-        {
-            for(int j = 0; j < img.height; j++)
-                printf("%.1f\t", img.r[array_matrix_index(img, i, j)]);
-            printf("\n");
-        }
-
-        printf("-------------------------------------------------------------- \n"); 
-
-        for(int i = 0; i < tmp.height; i++)
-        {
-            for(int j = 0; j < tmp.height; j++)
-                printf("T:%.1f\t", tmp.r[array_matrix_index(tmp, i, j)]);
-            printf("\n");
-        }
-
-        printf("-------------------------------------------------------------- \n"); 
-
-        */
 
         double sum = 0.0;
         for(int i = 0; i < img.height; i++)
@@ -74,29 +53,8 @@ int main(int argc, char ** argv)
             }
         }
        
-        /*
-         *
-        printf("-------------------------------------------------------------- \n"); 
+        save_image("filter_test.jpg", &tmp);
 
-        for(int i = 0; i < tmp.height; i++)
-        {
-            for(int j = 0; j < tmp.height; j++)
-                printf("T:%.1f\t", tmp.r[array_matrix_index(tmp, i, j)]);
-            printf("\n");
-        }
-
-        printf("-------------------------------------------------------------- \n"); 
-
-        for(int i = 0; i < img.height; i++)
-        {
-            for(int j = 0; j < img.height; j++)
-                printf("I:%.1f\t", img.r[array_matrix_index(img, i, j)]);
-            printf("\n");
-        }
-
-        */
-
-        save_image("aesthetics_vaporwave-out.jpg", &tmp);
 	free_image(&tmp);
         free_image(&img);
         return 0;
