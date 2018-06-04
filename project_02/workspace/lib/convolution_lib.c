40dc5a49c5876c6b159b4e5eecb09a9c73e1a36d
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-04 00:11:29 -0300

    Fixing the bug that the image row position was never changed (now it depends on the convolution row being calculated) and fixing the borders' limits. Beautifully working.

--- project_02/workspace/lib/convolution_lib.c
+++ project_02/workspace/lib/convolution_lib.c
@@ -20,8 +20,8 @@ uint64_t array_matrix_index(image img, uint16_t row, uint16_t column)
 
 // This function calculates a single row of a convolution between two matrices.
 // It is assumed that the row passed as an argument is valid
-double calculate_conv_row(image img, float * channel, image * tmp, conv_matrix
-        cm, uint16_t pixel_row, uint16_t pixel_column, uint16_t cm_row)
+double calculate_conv_row(image img, float * channel, conv_matrix cm, uint16_t
+        pixel_row, uint16_t pixel_column, uint16_t cm_row)
 {
     // Orthogonal distance from the borders of the convolution matrix to the
     // pixel under calcultation
@@ -34,7 +34,7 @@ double calculate_conv_row(image img, float * channel, image * tmp, conv_matrix
     currently_position img_position;
     // The convolution image row never changes in this context, since this
     // function calculates a single row of convolution operation
-    img_position.currently_row = pixel_row - distance;
+    img_position.currently_row = pixel_row - distance + cm_row;
     img_position.currently_column = pixel_column - distance;
 
     currently_position cm_position;
@@ -51,16 +51,16 @@ double calculate_conv_row(image img, float * channel, image * tmp, conv_matrix
         // the convolution operation is valid. We decided to ignore those cases
         // in the filter aplication. It'll cause a border-effect: fading pixels
         // near to the borders).
-        if(img_position.currently_row >= 0 &&
-                img_position.currently_column >= 0)
+        if(img_position.currently_row >= 0 && img_position.currently_row <
+                img.height && img_position.currently_column >= 0 &&
+                img_position.currently_column < img.width)
         {
             convolution_row_sum += channel[array_matrix_index(img,
                     img_position.currently_row, img_position.currently_column)]
-                    * cm.matrix[cm_position.currently_row][cm_position.currently_column];
+                * cm.matrix[cm_position.currently_row][cm_position.currently_column];
         }
         img_position.currently_column++;
         cm_position.currently_column++;
     }
     return convolution_row_sum;
 } 
-
