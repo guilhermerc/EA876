d68a25eb641ef17a068749b5c6a4b62e8004d27c
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-05 12:12:25 -0300

    Arguments for single(-s) or multi(-m) thread implemented. Also, if -m is the chosen argument, the user has to put the number of desired threads.

--- project_02/workspace/src/main.c
+++ project_02/workspace/src/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "image_io_lib.h"
 #include "convolution_lib.h"
+#include "multithread_filter_lib.h"
 
 int main(int argc, char ** argv)
 {
-        image img = open_image("tests/test.jpg");
+        img = open_image("tests/test.jpg");
 
-        image tmp;
         tmp.width = img.width;
         tmp.height = img.height;
         
@@ -20,8 +21,7 @@ int main(int argc, char ** argv)
             tmp.r[i] = 0; tmp.g[i] = 0; tmp.b[i] = 0;
         }
 
-        conv_matrix cm;
-        cm.order = 10; cm.divider = 100;
+        cm.order = 11; cm.divider = 121;
 
         cm.matrix = malloc(sizeof(float *) * cm.order);
         for(int i = 0; i < cm.order; i++)
@@ -33,27 +33,43 @@ int main(int argc, char ** argv)
                 cm.matrix[i][j] = 1;
         }
 
-        double sum = 0.0;
-        for(int i = 0; i < img.height; i++)
+        if(argc > 1)
         {
-            for(int j = 0; j < img.width; j++)
+            if(strcmp(argv[1], "-s") == 0)
+            {    
+                printf("Singlethread mode!\n");
+
+                double sum = 0.0;
+                for(int i = 0; i < img.height; i++)
+                {
+                    for(int j = 0; j < img.width; j++)
+                    {
+                        for(int k = 0; k < cm.order; k++)
+                            sum += calculate_conv_row(img, img.r, cm, i, j, k); 
+                        tmp.r[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
+                        sum = 0.0;
+                        for(int k = 0; k < cm.order; k++)
+                            sum += calculate_conv_row(img, img.g, cm, i, j, k); 
+                        tmp.g[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
+                        sum = 0.0;
+                        for(int k = 0; k < cm.order; k++)
+                            sum += calculate_conv_row(img, img.b, cm, i, j, k); 
+                        tmp.b[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
+                        sum = 0.0;
+                    }
+                }
+            }
+            else if(strcmp(argv[1], "-m") == 0)
             {
-                for(int k = 0; k < cm.order; k++)
-                    sum += calculate_conv_row(img, img.r, cm, i, j, k); 
-                tmp.r[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
-                sum = 0.0;
-                for(int k = 0; k < cm.order; k++)
-                    sum += calculate_conv_row(img, img.g, cm, i, j, k); 
-                tmp.g[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
-                sum = 0.0;
-                for(int k = 0; k < cm.order; k++)
-                    sum += calculate_conv_row(img, img.b, cm, i, j, k); 
-                tmp.b[array_matrix_index(tmp, i, j)] = (float) sum / cm.divider;
-                sum = 0.0;
+                N_THREADS = atoi(argv[2]);
+                printf("Multithread mode with %d threads!\n", N_THREADS);
+                multithread_filter();
             }
         }
-       
-        save_image("filter_test.jpg", &tmp);
+        else
+            printf("Nothing has been done here\n");
+
+        save_image("filter_test-out.jpg", &tmp);
 
 	free_image(&tmp);
         free_image(&img);
