7992b134e550beb17d532500cc7f0e0d594bced5
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-05 19:33:55 -0300

    Updating the multithread filter

--- project_02/workspace/lib/multithread_filter_lib.c
+++ project_02/workspace/lib/multithread_filter_lib.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
 
 #include "image_io_lib.h"
+#include "convolution_lib.h"
 #include "multithread_filter_lib.h"
 
 #include <pthread.h>
 #include <time.h>
 
-#define N_THREADS 4
+typedef enum {R, G, B} channel;
 
-typedef enum {RED, GREEN, BLUE} channel;
+void thread_conv(channel ch, uint8_t id);
 
 pthread_mutex_t lock_;
 
@@ -23,12 +23,14 @@ void * thread_func(void * arg)
 
     pthread_mutex_unlock(&lock_);
 
-    // (RED, id);
-    // (GREEN, id);
-    // (BLUE, id);
+    thread_conv(R, id); thread_conv(G, id); thread_conv(B, id);
 }
 
-void multithread_filter(image img, image tmp, conv_matrix cm)
+// This function applies filter given by the convolution operation of the
+// convolution matrix 'cm' by each pixel of the image 'img' (both are global
+// variables) using multiple (at least > 1) threads. The number of threads are
+// defined as an argument of main call.
+void multithread_filter()
 {
     pthread_t threads[N_THREADS];
     uint8_t threads_id[N_THREADS];
@@ -37,34 +39,60 @@ void multithread_filter(image img, image tmp, conv_matrix cm)
         threads_id[i] = i;
 
     for(int i = 0; i < N_THREADS; i++)
-        pthread_create(&(threads[i]), NULL, thread_func, (void *) (&(threads_id[i])));
+        pthread_create(&(threads[i]), NULL, thread_func, (void *)
+                (&(threads_id[i])));
 
     for(int i = 0; i < N_THREADS; i++)
         pthread_join(threads[i], NULL);
 }	
 
-/*
-    float * aux = NULL;
+// Each thread is responsible for calculating a certain number of dot products
+// between a convolution matrix row and a image row under a filtering operation.
+// These dot products contribute to a convolution operation result. This
+// function traverses an image's channel matrix (R, G or B) and calculates the
+// contribution of the lines in charge by the current thread to the convolution
+// operation and updates the image buffer 'tmp' (used to keep the under
+// construction image).
+void thread_conv(channel ch, uint8_t id) 
+{
+    float * img_channel = NULL;
+    float * tmp_channel = NULL;
     switch(ch)
     {
-        case RED:
+        case R:
             {
-                aux = img.r;
+                img_channel = img.r;
+                tmp_channel = tmp.r;
                 break;
             }
-        case GREEN:
+        case G:
             {
-                aux = img.g;
+                img_channel = img.g;
+                tmp_channel = tmp.g;
                 break;
             }
-        case BLUE:
+        case B:
             {
-                aux = img.b;
+                img_channel = img.b;
+                tmp_channel = tmp.b;
             }
-    }	
+    }
 
-    for(int k = id; k < convm.order; k += N_THREADS)
+    // Traversing the image matrix rows
+    for(uint16_t row = 0; row < img.height; row++)
     {
-        // calculate
-    }	
-*/
+        // Traversing the image matrix columns
+        for(uint16_t column = 0; column < img.width; column++)
+        {
+            // Trying to divide equally the number of dot products to each
+            // thread calculate. If the order of the convolution matrix is a
+            // multiple of the number of threads, they're balanced. If not,
+            // there will be threads doing 1 more row than others.
+            for(uint16_t thread_row = id; thread_row < cm.order; thread_row
+                    += N_THREADS)
+                tmp_channel[array_matrix_index(tmp, row, column)] += (float)
+                    calculate_conv_row(img, img_channel, cm, row, column,
+                            thread_row) / cm.divider; 
+        }
+    }
+}
