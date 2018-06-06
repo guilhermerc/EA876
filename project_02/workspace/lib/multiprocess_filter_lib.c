193eb4e8f3038d465977df8cccc3efcb55479205
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-06 12:08:53 -0300

    Implementing the result image as a shared memory between the processess (Working)

--- project_02/workspace/lib/multiprocess_filter_lib.c
+++ project_02/workspace/lib/multiprocess_filter_lib.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <sys/types.h>
+#include <stdbool.h>
+#include <sys/types.h>  // define pid_t
+#include <sys/mman.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <unistd.h>     // fork()
 
 //#include <sys/sysinfo.h>
 
@@ -14,38 +17,38 @@
 
 typedef enum {R, G, B} channel;
 
-void proc_conv(channel ch, uint8_t id) 
+void proc_conv(channel ch, uint8_t id, image * shared_tmp) 
 {
     float * img_channel = NULL;
-    float * tmp_channel = NULL;
+    float * shared_tmp_channel = NULL;
     switch(ch)
     {
         case R:
             {
                 img_channel = img.r;
-                tmp_channel = tmp.r;
+                shared_tmp_channel = shared_tmp->r;
                 break;
             }
         case G:
             {
                 img_channel = img.g;
-                tmp_channel = tmp.g;
+                shared_tmp_channel = shared_tmp->g;
                 break;
             }
         case B:
             {
                 img_channel = img.b;
-                tmp_channel = tmp.b;
+                shared_tmp_channel = shared_tmp->b;
             }
     }
     
     int i, j, p;
-    for(i = 0; i < img.height; i++)
+    for(i = 0; i < shared_tmp->height; i++)
     {
-        for(j = 0; j < img.width; j++)
+        for(j = 0; j < shared_tmp->width; j++)
         {
             for(p = id; p < cm.order; p+= N_PROCESSES)
-                tmp_channel[array_matrix_index(tmp, i, j)] += (float)
+                shared_tmp_channel[array_matrix_index(* shared_tmp, i, j)] += (float)
                     calculate_conv_row(img, img_channel, cm, i, j, p) /
                     cm.divider; 
 
@@ -58,14 +61,48 @@ void multiprocess_filter() {
     // N_PROCESSES = get_nprocs(); 
     pid_t pids[N_PROCESSES];
 
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANON;
+
+    image * shared_tmp = (image *) mmap(NULL, sizeof(image), protection,
+            visibility, 0, 0); 
+
+    shared_tmp->width = tmp.width;
+    shared_tmp->height = tmp.height;
+
+    shared_tmp->r = (float *) mmap(NULL, sizeof(float) * shared_tmp->width *
+            shared_tmp->height, protection, visibility, 0, 0);
+    shared_tmp->g = (float *) mmap(NULL, sizeof(float) * shared_tmp->width *
+            shared_tmp->height, protection, visibility, 0, 0);
+    shared_tmp->b = (float *) mmap(NULL, sizeof(float) * shared_tmp->width *
+            shared_tmp->height, protection, visibility, 0, 0);
+
+    /*
+    shared_tmp->r = (float *) malloc(sizeof(float) * shared_tmp->width *
+            shared_tmp->height);
+    shared_tmp->g = (float *) malloc(sizeof(float) * shared_tmp->width *
+            shared_tmp->height);
+    shared_tmp->b = (float *) malloc(sizeof(float) * shared_tmp->width *
+            shared_tmp->height);
+    */
+
+    for(int i = 0; i < shared_tmp->width * shared_tmp->height; i++) 
+    {
+        shared_tmp->r[i] = 0; shared_tmp->g[i] = 0; shared_tmp->b[i] = 0;
+    }
+
     int k;
     for (k = 0; k < N_PROCESSES; ++k) {
         pids[k] = fork();
         if (pids[k] == 0) {
-            proc_conv(R, k); proc_conv(G, k); proc_conv(B, k);
+            proc_conv(R, k, shared_tmp);
+            proc_conv(G, k, shared_tmp);
+            proc_conv(B, k, shared_tmp);
             exit(0);
         }
     }
 
     for (k = 0; k < N_PROCESSES; ++k)   waitpid(pids[k], NULL, 0);
+
+    tmp = * shared_tmp;
 }	
