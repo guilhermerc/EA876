8f55ccdfc192951d96e3c51b752b00a935826055
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-06 23:33:13 -0300

    Uploading the main program to print the desired output of this project

--- project_02/workspace/src/main.c
+++ project_02/workspace/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "image_io_lib.h"
@@ -11,7 +13,10 @@
 
 int main(int argc, char ** argv)
 {
-        img = open_image("imgs_test/test.jpg");
+        img = open_image(argv[1]);
+
+        printf("Arquivo da imagem: %s\n", argv[1]);
+        printf("Resolução: %d pixels x %d pixels\n", img.width, img.height);
 
         tmp.width = img.width;
         tmp.height = img.height;
@@ -26,6 +31,8 @@ int main(int argc, char ** argv)
 
         cm.order = 11; cm.divider = 121;
 
+        printf("Tamanho da matriz de convolução: %d x %d\n", cm.order, cm.order);
+
         cm.matrix = malloc(sizeof(float *) * cm.order);
         for(int i = 0; i < cm.order; i++)
             cm.matrix[i] = malloc(sizeof(float) * cm.order);
@@ -38,41 +45,67 @@ int main(int argc, char ** argv)
 
         if(argc > 1)
         {
-            if(strcmp(argv[1], "-s") == 0)
+            if(strcmp(argv[2], "-s") == 0)
             {    
-                printf("Singlethread mode!\n");
+                printf("Estratégia: singlethread\n");
                 time_measurement(singlethread_filter, NULL);
                 //singlethread_filter();
             }
-            else if(strcmp(argv[1], "-t") == 0)
+            else if(strcmp(argv[2], "-t") == 0)
             {
                 if(argc > 2)    
                 {
-                    N_THREADS = atoi(argv[2]);
-                    printf("Multithread mode with %d threads!\n", N_THREADS);
+                    N_THREADS = atoi(argv[3]);
+                    printf("Estratégia: multithread (%d threads)\n", N_THREADS);
                     time_measurement(multithread_filter, NULL);
-                    multithread_filter();
+                    //multithread_filter();
                 }
                 else
-                    printf("Invalid usage. Please, insert the number of threads as a third argument.\n");
+                    printf("Invalid usage. Please, insert the number of threads as a fourth argument.\n");
             }
-            else if(strcmp(argv[1], "-p") == 0)
+            else if(strcmp(argv[2], "-p") == 0)
             {
                 if(argc > 2)    
                 {
-                    N_PROCESSES = atoi(argv[2]);
-                    printf("Multiprocess mode with %d processes!\n", N_PROCESSES);
+                    N_PROCESSES = atoi(argv[3]);
+                    printf("Estratégia: multiprocesso (%d processos)\n", N_PROCESSES);
                     time_measurement(multiprocess_filter, NULL);
-                    multiprocess_filter();
+                    //multiprocess_filter();
                 }
                 else
-                    printf("Invalid usage. Please, insert the number of processes as a third argument.\n");
+                    printf("Invalid usage. Please, insert the number of processes as a fourth argument.\n");
             }
         }
         else
             printf("Nothing has been done here\n");
+        
+        uint8_t size = 0;
+        bool found = false;
+        for(size = 1; found == false; size++)
+        {
+            if(argv[1][size] == '\0') found = true;
+        }
 
-        save_image("filter_test-out.jpg", &tmp);
+        argv[1][size - 5] = '\0';
+
+        char output_name[200] = "out/";
+        strcat(output_name, argv[1]);
+        if(strcmp(argv[2], "-s") == 0)
+            strcat(output_name, "-singlethread");
+        else if(strcmp(argv[2], "-t") == 0)
+        {
+            strcat(output_name, "-multithread");
+            strcat(output_name, "-"); strcat(output_name, argv[3]);
+        }
+        else if(strcmp(argv[2], "-p") == 0)
+        {
+            strcat(output_name, "-multiprocess");
+            strcat(output_name, "-"); strcat(output_name, argv[3]);
+        }
+
+        strcat(output_name, "-out.jpg");    
+
+        save_image(output_name, &tmp);
 
 	//free_image(&tmp);
         free_image(&img);
