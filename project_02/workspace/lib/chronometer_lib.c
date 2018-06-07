ad5f26efb0b4e21599ee3f37c9abafe50ac90a09
guilhermerc <guilherme.riciolic@gmail.com>
2018-06-06 23:29:07 -0300

    Updating the library to print the execution time in miliseconds

--- project_02/workspace/lib/chronometer_lib.c
+++ project_02/workspace/lib/chronometer_lib.c
@@ -21,10 +21,8 @@ void time_measurement(void * (* function)(void * args), void * args)
 
   timersub(&real_time_1, &real_time_0, &real_time_diff);
 
-  printf("Real time: %ld.%06ld seconds\n", real_time_diff.tv_sec,
-          real_time_diff.tv_usec);
-  /*
-  printf("User time: %f seconds\n", (double) (processor_time_used_1 -
-              processor_time_used_0)/CLOCKS_PER_SEC);
-              */
+  int time_diff_ms = (real_time_diff.tv_sec * 1000) +
+      (real_time_diff.tv_usec / 1000);
+
+  printf("Tempo gasto:\t\t\t%d ms\n", time_diff_ms);
 }
